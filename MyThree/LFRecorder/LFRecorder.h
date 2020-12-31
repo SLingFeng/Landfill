eecae916047bcf5190813ab1f850700ba9e48390
lingfeng sun <slflibra@gmail.com>
2020-12-31 13:11:58 +0800

    录音

---
+++ MyThree/LFRecorder/LFRecorder.h
@@ -0,0 +1,17 @@
+//
+//  LFRecorder.h
+//  Test
+//
+//  Created by mrd on 2020/12/31.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LFRecorder : NSObject
+- (void)startRecorder;
+- (void)stopRecorder;
+@end
+
+NS_ASSUME_NONNULL_END
